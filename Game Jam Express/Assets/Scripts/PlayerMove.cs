12c37a2e83fa33872c0ec275b22ff384bc214a69
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2024-04-20 15:23:26 +1000

    Added coyote time and jump buffer.

--- Game Jam Express/Assets/Scripts/PlayerMove.cs
+++ Game Jam Express/Assets/Scripts/PlayerMove.cs
@@ -8,12 +8,20 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float moveSpeed = 10;
     [SerializeField] private float jumpSpeed = 25;
     [SerializeField] private float jumpSpin = 360;
+    [SerializeField] private float restoreSpin = 180;
+
+    [SerializeField] private Vector3 groundCheckPoint;
+    [SerializeField] private float groundCheckRadius = 0.1f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float coyoteTime = 0.1f;
 
     private Actions actions;
     private InputAction moveAction;
     private InputAction jumpAction;
-    private Rigidbody2D rigidbody;
+    new private Rigidbody2D rigidbody;
     private AudioSource jumpAudio;
+    private float wasOnGround = float.NegativeInfinity;
+    private float wasJumpPressed = float.NegativeInfinity;
 
     void Awake()
     {
@@ -37,22 +45,57 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+        CheckOnGround();
+
+        if (jumpAction.WasPerformedThisFrame()) {
+            wasJumpPressed = Time.time;
+        }
+
         Vector2 move = moveAction.ReadValue<Vector2>();
         Vector3 velocity = rigidbody.velocity;
         velocity.x = move.x * moveSpeed;
 
-        if (jumpAction.WasPerformedThisFrame()) 
+        if (Time.time - wasOnGround < coyoteTime) 
         {
-            jumpAudio.Play();
-            velocity.y = jumpSpeed;
-            rigidbody.angularVelocity = jumpSpin * move.x;
+            if (Time.time - wasJumpPressed < coyoteTime) {
+                jumpAudio.Play();
+                velocity.y = jumpSpeed;
+                rigidbody.angularVelocity = jumpSpin * move.x;
+            }
+            
+            if (velocity.y <= 0) {
+                // add force to return to upright
+                float angle = transform.localEulerAngles.z;
+                angle = (angle + 180) % 360 - 180;  // in range -180 to 180
+                rigidbody.angularVelocity = -restoreSpin * angle / 180;
+            }
         }
 
         rigidbody.velocity = velocity;
     }
 
+    private void Jump() 
+    {
+    }
+
+
+    private void CheckOnGround() {
+        Vector2 point = transform.position + groundCheckPoint;
+        Collider2D collider = Physics2D.OverlapCircle(point, groundCheckRadius, groundLayer); 
+        if (collider != null)
+        {
+            wasOnGround = Time.time;
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D collider) {
 
+    }
+
+    void OnDrawGizmos() {
+        bool onGround = (Time.time - wasOnGround < coyoteTime);
+        Gizmos.color = (onGround  ? Color.green : Color.yellow);
+        Gizmos.DrawSphere(transform.position + groundCheckPoint, groundCheckRadius);
     }
 
 }
